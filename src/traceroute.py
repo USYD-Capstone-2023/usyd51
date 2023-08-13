979b16bbdffbb26b9d3bf1bebd0a514bdec82bb1
Leo Denham <leoleodenham@gmail.com>
2023-08-13 20:24:48 +1000

    Added a new script to do a basic visualisation of the trip between your computer and a destination server

--- src/traceroute.py
+++ src/traceroute.py
@@ -8,69 +8,78 @@ import struct
 port = 33434
 max_hops = 30
 
-# Initialize sockets
-icmp_proto = socket.getprotobyname("icmp")
-icmp_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp_proto)
+def get_ip_route(dest_ip):
+    # Initialize sockets
+    icmp_proto = socket.getprotobyname("icmp")
+    icmp_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp_proto)
 
-udp_proto = socket.getprotobyname("udp")
-udp_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM, udp_proto)
+    udp_proto = socket.getprotobyname("udp")
+    udp_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM, udp_proto)
 
-# Request timeout information
-timeout_data = struct.pack("ll", 1, 0)
+    # Request timeout information
+    timeout_data = struct.pack("ll", 1, 0)
 
-icmp_socket.bind(("", port))
-icmp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVTIMEO, timeout_data)
+    icmp_socket.bind(("", port))
+    icmp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVTIMEO, timeout_data)
 
-# Temporary target for basic implementation, final version will do a ping sweep to get all connected local addresses, then trace
-# the route to those. 
-target = "google.com"
-target_ip = socket.gethostbyname(target)
+    # Temporary target for basic implementation, final version will do a ping sweep to get all connected local addresses, then trace
+    # the route to those. 
+    target = dest_ip
+    target_ip = socket.gethostbyname(target)
 
-print(f"Tracing route from {socket.gethostname()} to {target_ip} ({target})")
+    # print(f"Tracing route from {socket.gethostname()} to {target_ip} ({target})")
 
-ttl = 1
-found_target = False
-while ttl < max_hops and not found_target:
+    ttl = 1
+    found_target = False
 
-    # Sends a message on UDP to the target with a limited ttl
-    udp_socket.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
-    udp_socket.sendto(b"", (target_ip, port))
 
-    attempts = 5
-    responded = False
 
-    # Awaits an ICMP response from a router on the path stating that ttl has expired, or the goal has been reached
-    while attempts > 0:
+    # Generate a list of ip-addresses and hostnames while traversing
+    found = []
 
-        # Gets information of the router that responded and prints it
-        try:
-            responded = True
-            pack, ip = icmp_socket.recvfrom(512)
-        except socket.error:
-            attempts -= 1
-            continue
-    
-        if responded:
-            host_name = "UNKNOWN"
+    while ttl < max_hops and not found_target:
+
+        # Sends a message on UDP to the target with a limited ttl
+        udp_socket.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
+        udp_socket.sendto(b"", (target_ip, port))
+
+        attempts = 5
+        responded = False
+
+        # Awaits an ICMP response from a router on the path stating that ttl has expired, or the goal has been reached
+        while attempts > 0:
+
+            # Gets information of the router that responded and prints it
             try:
-                # Resolves hostname if possible. 
-                # As far as i can tell this will be a bit more difficult on LAN
-                host_name = socket.gethostbyaddr(ip[0])[0]
-            except:
-                pass
-
-            print(f"Hopped to {ip[0]} ({host_name}); ttl: {ttl}")
-           
-            # Ends if the target was reached
-            if ip[0] == target_ip:
-                found_target = True
+                responded = True
+                pack, ip = icmp_socket.recvfrom(512)
+            except socket.error:
+                attempts -= 1
+                continue
         
-            break 
+            if responded:
+                host_name = "UNKNOWN"
+                try:
+                    # Resolves hostname if possible. 
+                    # As far as i can tell this will be a bit more difficult on LAN
+                    host_name = socket.gethostbyaddr(ip[0])[0]
+                except:
+                    pass
 
-    # Increments ttl to get the next router in the path
-    ttl += 1
+                # print(f"Hopped to {ip[0]} ({host_name}); ttl: {ttl}")
+                found.append((ip[0], host_name));
+            
+                # Ends if the target was reached
+                if ip[0] == target_ip:
+                    found_target = True
+            
+                break 
 
-print(f"Reached target in {ttl} hops")
+        # Increments ttl to get the next router in the path
+        ttl += 1
+
+    # print(f"Reached target in {ttl} hops")
+    return found
 
 
 
---
+++ src/visualise.py
@@ -0,0 +1,39 @@
+import traceroute
+import igraph as ig
+import matplotlib.pyplot as plt
+
+
+destination = input("Enter the ip address or url of the destination: ")
+if (destination == ""):
+    destination = "google.com"
+    print("No IP entered, defaulting to google.com")
+locations = traceroute.get_ip_route(destination)
+n_vertices = len(locations)
+edges = [(i, i+1) for i in range(len(locations)-1)]
+edges.append((0,len(locations)-1))
+
+
+
+graph = ig.Graph(n_vertices, edges)
+
+graph["title"] = "Network Visualisation"
+
+# Vertex name is hostname if found, otherwise make it the IP of the node.
+graph.vs["name"] = [x[1] if x[1] != "UNKNOWN" else x[0] for x in locations]
+
+fig, ax = plt.subplots(figsize=(5,5))
+ig.plot(
+    graph,
+    target=ax,
+    vertex_size=0.1,
+    vertex_frame_width=4.0,
+    vertex_frame_color='white',
+    vertex_label=graph.vs["name"],
+    vertex_label_size=7.0,
+    edge_color='black'
+)
+
+plt.show()
+
+
+
