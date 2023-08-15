14a85b13063e66d6de4be9ffa4f587a2de4e1472
sam <samkelly1290@gmail.com>
2023-08-15 12:55:55 +1000

    fixed key errors with devices found during traceroute that arent in the network, changed graph formatting, optimised with scapy traceroute func

--- src/scapy_network_visualise.py
+++ src/scapy_network_visualise.py
@@ -3,9 +3,6 @@ import igraph as ig
 import matplotlib.pyplot as plt
 import re, socket
 
-MAX_HOPS = 10
-GATEWAY = "192.168.1.1"
-
 def get_clients(ip_range):
 
     print("Getting active devices on local network...")
@@ -31,32 +28,36 @@ def get_clients(ip_range):
 
     print("Discovered %d devices!\nResolving hostnames..." % (len(responded)))
     # Gather and store all relevant data for current client
+    cursor = 0
     for response in responded:
 
         ip = response[1].psrc
         mac = response[1].hwsrc
 
-        # resolves hostname if possible
+        # resolves hostname if possible. Massive time cost here, need to find a better solution +++
         name = ip
-        try:
-            name = socket.gethostbyaddr(ip)[0]
-        except:
-            pass
+        # try:
+        #     name = socket.gethostbyaddr(ip)[0]
+        # except:
+        #     pass
 
+        print("[%d] Name is %s" % (cursor, name))
+        cursor += 1
         clients[ip] = {"mac" : mac, "name" : name, "route" : []}
 
     return clients
 
 # Gets the ip of all devices on the path from the host to the target ip
-def tcp_traceroute(ip, gateway):
+def tcp_traceroute(ip):
 
     print("Tracing route to %s" % (ip))
 
     # Runs traceroute.
     # Emits TCP packets with incrementing ttl until the target is reached
-    answers = scapy.sr(scapy.IP(dst=ip, ttl=(1, MAX_HOPS))/scapy.TCP(dport=53, flags="S"), timeout=3, verbose=False)[0]
+    # answers = scapy.sr(scapy.IP(dst=ip, ttl=(1, MAX_HOPS))/scapy.TCP(dport=53, flags="S"), timeout=3, verbose=False)[0]
+    answers = scapy.traceroute(ip, verbose=False)[0]
 
-    addrs = [gateway]
+    addrs = []
     for response in answers:
 
         ip = response.answer.src
@@ -79,45 +80,73 @@ def draw_graph(edges, vertices):
     # Vertex name is hostname if found, otherwise make it the IP of the node.
     graph.vs["name"] = vertices
 
-    fig, ax = plt.subplots(figsize=(5,5))
+    fig, ax = plt.subplots(figsize=(20, 20))
     ig.plot(
         graph,
+        mode="mutual",
         target=ax,
-        vertex_size=0.1,
+        vertex_size=0.001,
         vertex_frame_width=4.0,
         vertex_frame_color='white',
         vertex_label=graph.vs["name"],
         vertex_label_size=7.0,
-        vertex_label_dist=3.0,
-        edge_color='black'
-
+        vertex_label_dist=1.0,
+        edge_color='black',
+        edge_width=0.1
     )
 
+    fig.savefig("output.png")
     plt.show()
 
 if __name__ == "__main__":
 
     clients = get_clients("192.168.1.0/24")
 
+    route_clients = {}
     vertices = []
-    for client in clients.keys():
-        vertices.append(clients[client]["name"])
-
     edges = set()
     for client in clients.keys():
 
         data = clients.get(client)
-        route = tcp_traceroute(client, GATEWAY)
+        route = tcp_traceroute(client)
         data["route"].extend(route)
 
-        prev_client_name = None
+        # Creates a dictionary of all clients including those found in the path
+        route_clients[client] = data
+        # Generates all vertices
         for ip in data["route"]:
+            if ip in route_clients.keys():
+                continue
 
-            client_name = clients[ip]["name"]
+            elif ip in clients.keys():
+                route_clients[ip] = clients.get(ip)
+                route_clients[ip]["route"].extend(route)
+                vertices.append(clients[ip]["name"])
+
+            else:
+                route_clients[ip] = {"mac" : "UNKNOWN", "name" : ip, "route" : route}
+                vertices.append(ip)
+            
+    # Generates all edges
+    for client in route_clients:
+        prev_client_name = None
+        for ip in route_clients.get(client)["route"]:
+
+            client_name = ip
+            if ip in route_clients.keys():
+                client_name = route_clients[ip]["name"]
 
             if prev_client_name and prev_client_name != client_name:
+                if client_name not in vertices:
+                    vertices.append(client_name)
 
-                edges.add((vertices.index(prev_client_name), vertices.index(client_name)))
+                if prev_client_name not in vertices:
+                    vertices.append(prev_client_name)
+
+                i1 = vertices.index(prev_client_name)
+                i2 = vertices.index(client_name)
+
+                edges.add((min(i1, i2), max(i1, i2)))
 
             prev_client_name = client_name
 
