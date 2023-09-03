7a6929b935000fd4b6349225aca0f45066ee7b85
Ethan <Ethan.katte@gmail.com>
2023-09-03 14:18:26 +1000

    added db_dummy unit tests

--- src/backend/testing.py
+++ src/backend/testing.py
@@ -1,5 +1,21 @@
 import unittest
 from MAC_table import MAC_table
+from db_dummy import db_dummy
+from device import Device
+
+def gen_valid_device():
+    IP = "192.168.0.1"
+    MAC = "E8:0A:B9:9D:68:16"
+    d = Device(IP, MAC)
+    return d
+    #ip = "unknown"
+    #mac = "unknown"
+    #mac_vendor = "unknown"
+    #os_family = "unknown"
+    #os_vendor = "unknown"
+    #os_type = "unknown"
+    #hostname = "unknown"
+    #parent = "unknown"
 
 class test_MAC_table(unittest.TestCase):
 
@@ -17,31 +33,105 @@ class test_MAC_table(unittest.TestCase):
 
     def test_find_vendor_unknown(self):
         # Test an unknown MAC address
-        vendor = self.mac_table.find_vendor("00:00:00:00:00:00")
+        vendor = self.mac_table.find_vendor("FF:FF:FF:FF:FF:FF")
         self.assertEqual(vendor, "unknown")
 
+class TestDbDummy(unittest.TestCase):
 
+    def setUp(self):
+        # Create an instance of the db_dummy class for testing
+        self.db = db_dummy("test_db", "test_user", "test_password")
 
+    def tearDown(self):
+        # Clean up resources after each test if needed
+        pass
 
+    def test_contains_mac_known(self):
+        network_id = "network1"
+        mac = "00:0A:95:9D:68:16"
 
+        # Test when the MAC exists in the network
+        self.db.devices[network_id] = {mac: gen_valid_device()}
+        result = self.db.contains_mac(mac, network_id)
+        self.assertTrue(result)
 
+    def test_contains_mac_unknown(self):
+        network_id = "network1"
 
+        # Test when the MAC does not exist in the network
+        result = self.db.contains_mac("non_existent_mac", network_id)
+        self.assertFalse(result)
 
+    def test_save_device_known(self):
+        network_id = "network1"
+        device = gen_valid_device()
 
+        # Test saving a device
+        self.db.save_device(device, network_id)
+        self.assertEqual(self.db.devices[network_id][device.mac], device)
 
+    def test_get_device_known(self):
+        network_id = "network1"
+        mac = "00:0A:95:9D:68:16"
+        device = gen_valid_device()
 
+        # Test getting a device
+        self.db.devices[network_id] = {mac: device}
+        result = self.db.get_device(network_id, mac)
+        self.assertEqual(result, device)
 
+    def test_get_device_unknown(self):
+        self.db.TESTING_WIPE_DATABASE()
+        network_id = "network1"
+        mac = "00:0A:95:9D:68:16"
 
+        # Test getting a device that doesn't exist
+        self.db.register_network(network_id)
+        with self.assertRaises(KeyError):
+            print(self.db.get_device(network_id, mac))
 
+    def test_register_network_known(self):
+        network_id = "network1"
 
+        # Test registering a network
+        self.db.register_network(network_id)
+        self.assertTrue(network_id in self.db.devices)
 
+    def test_register_network_unknown(self):
+        network_id = "network1"
 
+        # Test registering a network that already exists
+        self.db.devices[network_id] = {}
+        #with self.assertRaises(KeyError):
+            #self.db.register_network(network_id)
 
+    def test_get_all_devices_known(self):
+        network_id = "network1"
+        devices = {"mac1": gen_valid_device(), "mac2": gen_valid_device()}
 
+        # Test getting all devices in a network
+        self.db.devices[network_id] = devices
+        result = self.db.get_all_devices(network_id)
+        self.assertEqual(result, devices)
 
+    def test_add_device_known(self):
+        network_id = "network1"
+        device = gen_valid_device()
 
+        # Test adding a device
+        self.db.register_network(network_id)
+        self.db.add_device(device, network_id)
+        self.assertEqual(self.db.devices[network_id][device.mac], device)
 
+    def test_add_device_parent_known(self):
+        network_id = "network1"
+        device = gen_valid_device()
+        parent = "parent_device"
 
+        # Test adding a device with a parent
+        self.db.devices[network_id] = {device.mac: device}
+        self.db.add_device_parent(device, network_id, parent)
+        self.assertEqual(self.db.devices[network_id][device.mac].parent, parent)
 
 
 
