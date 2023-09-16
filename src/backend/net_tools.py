77904d6b962480a71b2a1f6e9b74d35953804812
Ethan <Ethan.katte@gmail.com>
2023-09-16 11:52:56 +1000

    added port handling functions to front/back only prints to stdout right now as we dont have a local config file

--- src/backend/net_tools.py
+++ src/backend/net_tools.py
@@ -747,7 +747,7 @@ class Net_tools:
         return out
 
 
-    def add_ports(self, network_id, tcp=True, udp=True):
+    def add_ports(self, network_id, tcp=True, udp=True, ports=[]):
 
         devices = self.db.get_all_devices(network_id)
 
