fa4a12d9e09fba2b4322489778fd0d93e23053b3
Aryan Pokharna <aryanpokharna@outlook.com>
2023-09-20 11:46:16 +1000

    removed shadow from button

--- src/react-frontend/src/components/sidebar.tsx
+++ src/react-frontend/src/components/sidebar.tsx
@@ -4,20 +4,20 @@ import { Link } from "react-router-dom";
 
 const Sidebar = (props: any) => {
     return (
-        <div className="flex justify-between align-center flex-col w-1/8 bg-gray-200 opacity-75 rounded-xl">
+        <div className="flex justify-between align-center flex-col w-1/8 bg-gray-200 opacity-75 rounded-xl text-gray-900">
             <div className="flex flex-col justify-start items-center">
                 <Link to="/">
-                    <Button className="py-10">Home</Button>
+                    <Button className="py-10 text-gray-900 shadow-none">Home</Button>
                 </Link>
                 <Link to="/listView">
-                    <Button className="py-10">Networks</Button>
+                    <Button className="py-10 text-gray-900 shadow-none">Networks</Button>
                 </Link>
                 <Link to="/Settings">
-                    <Button className="py-10">Settings</Button>
+                    <Button className="py-10 text-gray-900 shadow-none">Settings</Button>
                 </Link>
 
-                <Button className="py-10">Analytics</Button>
-                <Button className="py-10">Night Mode</Button>
+                <Button className="py-10 shadow-none">Analytics</Button>
+                <Button className="py-10 shadow-none">Night Mode</Button>
             </div>
             <div className="flex flex-col justify-end items-center py-5">
                 <Avatar className="mb-6">
