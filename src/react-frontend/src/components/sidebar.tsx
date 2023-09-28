e785ce4a42afe6e0d90e6e3bb104f94d679873c2
Leo Denham <leo@index.inc>
2023-09-28 13:42:43 +1000

    Added icons to dashboard, fixed background colour, added rounded sidebar colours and card-ified dashboard

--- src/react-frontend/src/components/sidebar.tsx
+++ src/react-frontend/src/components/sidebar.tsx
@@ -1,33 +1,50 @@
 import { Button } from "@/components/ui/button";
 import { DarkMode } from "@/components/DarkModeButton";
 import { Link } from "react-router-dom";
-import { LayoutDashboard, Network, Cog, BarChart2, LogOut, UserCircle} from "lucide-react"
+import {
+  LayoutDashboard,
+  Network,
+  Cog,
+  BarChart2,
+  LogOut,
+  UserCircle,
+} from "lucide-react";
 
 const Sidebar = (props: any) => {
-    return (
-        <div className="flex justify-between align-center flex-col w-1/8 bg-gray-200 opacity-75 rounded-xl text-gray-900">
-            <div className="flex flex-col justify-start items-center">
-                <Link to="/">
-                    <Button className="py-10 text-gray-900 shadow-none">
-                        <LayoutDashboard></LayoutDashboard>
-                    </Button>
-                </Link>
-                <Link to="/ListView">
-                    <Button className="py-10 text-gray-900 shadow-none"><Network></Network></Button>
-                </Link>
-                <Link to="/Settings">
-                    <Button className="py-10 text-gray-900 shadow-none"><Cog></Cog></Button>
-                </Link>
+  return (
+    <div className="flex justify-between align-center flex-col w-1/8 bg-gray-200 opacity-75 rounded-xl text-gray-900 overflow-hidden">
+      <div className="flex flex-col justify-start items-center">
+        <Link to="/">
+          <Button className="py-10 text-gray-900 shadow-none">
+            <LayoutDashboard></LayoutDashboard>
+          </Button>
+        </Link>
+        <Link to="/ListView">
+          <Button className="py-10 text-gray-900 shadow-none">
+            <Network></Network>
+          </Button>
+        </Link>
+        <Link to="/Settings">
+          <Button className="py-10 text-gray-900 shadow-none">
+            <Cog></Cog>
+          </Button>
+        </Link>
 
-                <Button className="py-10 text-gray-900 shadow-none"><BarChart2></BarChart2></Button>
-                <DarkMode></DarkMode>
-            </div>
-            <div className="flex flex-col justify-end items-center py-5">
-                <Button className="py-10 text-gray-900 shadow-none"><UserCircle></UserCircle></Button>
-                <Button className="py-10 text-gray-900 shadow-none"><LogOut></LogOut></Button>
-            </div>
-        </div>
-    );
+        <Button className="py-10 text-gray-900 shadow-none">
+          <BarChart2></BarChart2>
+        </Button>
+        <DarkMode></DarkMode>
+      </div>
+      <div className="flex flex-col justify-end items-center">
+        <Button className="py-10 text-gray-900 shadow-none">
+          <UserCircle></UserCircle>
+        </Button>
+        <Button className="py-10 text-gray-900 shadow-none">
+          <LogOut></LogOut>
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
