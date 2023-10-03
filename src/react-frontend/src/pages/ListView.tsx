e6d316f45cf267ab423a577c698a479000955df6
Aryan Pokharna <aryanpokharna@outlook.com>
2023-10-03 12:33:57 +1100

    dark view in list view

--- src/react-frontend/src/pages/ListView.tsx
+++ src/react-frontend/src/pages/ListView.tsx
@@ -3,7 +3,7 @@ import Table from "@/components/table/page";
 
 const ListView = () => {
     return (        
-        <div className="flex w-full h-full" >
+        <div className="flex w-full h-full dark:bg-black" >
             <Sidebar />
             <Table />
         </div>
