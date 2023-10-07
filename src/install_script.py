7a994884ead9b3eee3d04707a8821fa43ad02e95
sam <skel4720@uni.sydney.edu.au>
2023-10-07 19:38:36 +1100

    remove old test file

--- src/install_script.py
+++ src/install_script.py
@@ -1,7 +1,7 @@
 import sys
 import subprocess
 
-requirements = ["flask", "scapy", "python-nmap", "netifaces", "requests"]
+requirements = ["flask", "scapy", "python-nmap", "netifaces", "requests", "pyjwt", "flask-cors", "unittest"]
 
 for req in requirements:
     subprocess.check_call([sys.executable, "-m", "pip", "install", req])
