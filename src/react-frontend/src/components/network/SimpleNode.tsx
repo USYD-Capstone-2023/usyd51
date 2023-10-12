50217fdfb91d562a9037df2ae2885e3d95364a4c
Leo Denham <leo@index.inc>
2023-10-12 14:13:42 +1100

    Added share network dropdown

---
+++ src/react-frontend/src/components/network/SimpleNode.tsx
@@ -0,0 +1,66 @@
+import { Handle, Position } from "reactflow";
+import { Card, CardContent, CardHeader } from "../ui/card";
+
+const SimpleNode = ({
+  data,
+  isConnectable,
+  selected,
+}: {
+  data: any;
+  isConnectable: boolean;
+  selected: boolean;
+}) => {
+  return (
+    <div className="text-updater-node">
+      <Handle
+        type="target"
+        position={Position.Top}
+        isConnectable={isConnectable}
+      />
+      <Card>
+        <CardHeader>{data.hostname}</CardHeader>
+        {selected && (
+          <CardContent>
+            <div>
+              <ul>
+                <li>MAC: {data.mac}</li>
+                <li>IP: {data.ip}</li>
+                <li>MAC Vendor: {data.mac_vendor}</li>
+                <li>OS Family: {data.os_family}</li>
+                <li>OS Vendor: {data.os_vendor}</li>
+                <li>OS Type: {data.os_type}</li>
+                <li>Hostname: {data.hostname}</li>
+                <li>Parent: {data.parent}</li>
+                {data.ports !== undefined && (
+                  <li>
+                    Ports:{" "}
+                    {data.ports.map((port: string, i: number) => (
+                      <span key={i}>
+                        {port}
+                        {i < data.ports.length - 1 ? ", " : ""}
+                      </span>
+                    ))}
+                  </li>
+                )}
+              </ul>
+            </div>
+          </CardContent>
+        )}
+      </Card>
+      <Handle
+        type="source"
+        position={Position.Bottom}
+        id="a"
+        isConnectable={isConnectable}
+      />
+      <Handle
+        type="source"
+        position={Position.Top}
+        id="b"
+        isConnectable={isConnectable}
+      />
+    </div>
+  );
+};
+
+export default SimpleNode;
