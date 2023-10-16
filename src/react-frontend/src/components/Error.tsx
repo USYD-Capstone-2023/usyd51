a33610128f0122647cea398d0edc267ce5b87765
Ethan <Ethan.katte@gmail.com>
2023-10-16 14:54:05 +1100

    changed error styling to shadcn framework, added useful error catching

--- src/react-frontend/src/components/Error.tsx
+++ src/react-frontend/src/components/Error.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import {
+  Alert,
+  AlertDescription,
+} from "@/components/ui/alert"
+import { AlertCircle } from "lucide-react"
 
 interface Props {
   message: string | null;
@@ -10,10 +15,13 @@ const ErrorNotification: React.FC<Props> = ({ message, clearError }) => {
   setTimeout(clearError, 5000);
 
   return (
-    <div className="error-notification">
-      {message}
-      <button onClick={clearError}>Close</button>
-    </div>
+    <Alert variant="destructive" className="error-notification fixed top-5 w-200 right-0 z-10 mr-12 bg-white">
+      <AlertCircle className="h-6 w-6 mt-1" />
+      <AlertDescription style={{ marginTop: 6}}>
+        <b>ERROR:</b> {message}
+        <button onClick={clearError} className="ml-5 bg-red-500 text-white p-2"><b>Close</b></button>
+      </AlertDescription>
+    </Alert>
   );
 };
 
