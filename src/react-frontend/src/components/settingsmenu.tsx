7d28aa16529c1d0c4a8f47f8820b6ab3adeb6161
James Thwaites <jamesthwaites03@gmail.com>
2023-10-17 13:34:49 +1100

    added daemon settings and scan interval input todo: update backend and uncomment links for d_ports and d_scanrate

--- src/react-frontend/src/components/settingsmenu.tsx
+++ src/react-frontend/src/components/settingsmenu.tsx
@@ -16,8 +16,6 @@ import { Switch } from "@/components/ui/switch";
 import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
 import { databaseUrl, scannerUrl } from "@/servers";
-import { ToastAction } from "@/components/ui/toast"
-import { useToast } from "@/components/ui/use-toast";
 import {
   Select,
   SelectContent,
@@ -60,7 +58,7 @@ const SettingsSwitch = (props: any) => {
           const authToken = localStorage.getItem("Auth-Token");
           if (authToken == null) {
             console.log("User is logged out!");
-            return;
+            return; 
           }
 
           const options = {
@@ -94,8 +92,6 @@ const SettingsSwitch = (props: any) => {
 };
 
 const SettingsMenu = (props: any) => {
-  const { toast } = useToast();
-
   // Retrieves stored settings from database
   useEffect(() => {
     const authToken = localStorage.getItem("Auth-Token");
@@ -152,23 +148,23 @@ const SettingsMenu = (props: any) => {
   );
 
   // DAEMON SETTINGS
-  const [d_scanrateSetting, setdScanrate] = useState(settings_json["UDP"]);
-  const [d_udpSetting, setcUDP] = useState(settings_json["UDP"]);
-  const [d_tcpSetting, setcTCP] = useState(settings_json["TCP"]);
-  const [d_portsSetting, setcPorts] = useState(settings_json["ports"]);
-  const [d_osSetting, setdOS] = useState(settings_json["run_os"]);
+  const [d_scanrateSetting, setdScanrate] = useState(0);
+  //const [d_scanrateSetting, setdScanrate] = useState(settings_json["d_scan_rate"]);
+  const [d_udpSetting, setdUDP] = useState(settings_json["d_UDP"]);
+  const [d_tcpSetting, setdTCP] = useState(settings_json["d_TCP"]);
+  const [d_portsSetting, setdPorts] = useState([80]);
+  const [d_osSetting, setdOS] = useState(settings_json["d_run_os"]);
   const [d_hostnameSetting, setdHostname] = useState(
-    settings_json["run_hostname"]
+    settings_json["d_run_hostname"]
   );
   const [d_macvendorSetting, setdMacVendor] = useState(
-    settings_json["run_mac_vendor"]
+    settings_json["d_run_mac_vendor"]
   );
   const [d_verttraceSetting, setdVertTrace] = useState(
-    settings_json["run_vertical_trace"]
+    settings_json["d_run_vertical_trace"]
   );
-  const [d_defaultviewSetting, setdDefaultView] = useState(
-    settings_json["defaultView"]
-  );
+
+  const [ scanIP, setScanIP ] = useState(settings_json["scanserver_ip"]);
 
   useEffect(() => {
     const authToken = localStorage.getItem("Auth-Token");
@@ -201,6 +197,17 @@ const SettingsMenu = (props: any) => {
           setMacVendor(data["content"]["run_mac_vendor"]);
           setVertTrace(data["content"]["run_vertical_trace"]);
           setPorts(data["content"]["ports"]);
+
+          //setdScanrate(data["content"]["d_scan_rate"]);
+          setdUDP(data["content"]["d_UDP"]);
+          setdTCP(data["content"]["d_TCP"]);
+          setdOS(data["content"]["d_run_os"]);
+          setdHostname(data["content"]["d_run_hostname"]);
+          setdMacVendor(data["content"]["d_run_mac_vendor"]);
+          setdVertTrace(data["content"]["d_run_vertical_trace"]);
+          //setdPorts(data["content"]["d_ports"]);
+
+          setScanIP(data["content"]["scanserver_ip"]);
         } else {
           throwCustomError(data["status"] + " " + data["message"]);
         }
@@ -225,7 +232,6 @@ const SettingsMenu = (props: any) => {
                 </CardHeader>
                 <CardContent>
                 
-                  
                   <Card>
                     <CardHeader>
                       <CardTitle className={cn("text-left")}>Port Scanning</CardTitle>
@@ -290,11 +296,6 @@ const SettingsMenu = (props: any) => {
                               <Input id="newport" type="email" placeholder="Enter port #..." />
                               <Button variant="outline" onClick={() => {
 
-                                toast({
-                                  title: "Scheduled: Catch up ",
-                                  description: "Friday, February 10, 2023 at 5:57 PM"
-                                })
-
                                 let val = parseInt(document.getElementById("newport").value);
                                 if (Number.isInteger(val)) {
                                   if(val >= 0 && val <= 65535) {
@@ -326,8 +327,11 @@ const SettingsMenu = (props: any) => {
                                       })
                                     );
                                     
+                                  } else {
+                                    throwCustomError("Enter a port between 0 and 65535");
                                   }
                                 } else {
+                                  throwCustomError("Enter an integer");
                                 }
                               }}
                               >
@@ -338,6 +342,9 @@ const SettingsMenu = (props: any) => {
                           </div>
                         </PopoverContent>
                       </Popover>
+
+
+                      
                     </div>
                     </CardContent>
                   </Card>
@@ -366,6 +373,199 @@ const SettingsMenu = (props: any) => {
                       c={verttraceSetting}
                       onc={setVertTrace}
                     />
+
+                  </div>             
+                </CardContent>
+              </Card>
+
+              <Card className={cn("w-full")}>
+                <CardHeader>
+                  <CardTitle className={cn("text-left")}>Daemon Settings</CardTitle>
+                </CardHeader>
+                <CardContent>
+                
+                  <Card>
+                    <CardHeader>
+                      <CardTitle className={cn("text-left")}>Port Scanning</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                    <div className="flex justify-start items-center flex-wrap">
+                      <SettingsSwitch
+                        switchName="TCP"
+                        settingname="TCP"
+                        c={d_tcpSetting}
+                        onc={setdTCP}
+                      />
+                      <SettingsSwitch
+                        switchName="UDP"
+                        settingname="UDP"
+                        c={d_udpSetting}
+                        onc={setdUDP}
+                      />
+                      <Popover>
+                        <PopoverTrigger asChild className={cn("w-1/3")}><Button className={cn("justify-between")} variant="outline">{"Ports: " + d_portsSetting.join(', ')}<ChevronsUpDown className="ml-2 h-4 w-4 shrink-0" /></Button></PopoverTrigger>
+                        <PopoverContent>
+                          <div className="flex flex-col justify-start space-y-1">
+                          {d_portsSetting.map((port) => (
+                            <Button key={port} className={cn("justify-between w-full")} variant="outline"
+                            onClick={() => {
+
+                              settings_json["d_ports"].splice(settings_json["d_ports"].indexOf(port), 1);
+                              setPorts([...settings_json["d_ports"]]);
+
+
+                              const authToken = localStorage.getItem("Auth-Token");
+                              if (authToken == null) {
+                                console.log("User is logged out!");
+                                return;
+                              }
+
+                              const options = {
+                                method: "PUT",
+                                headers: {
+                                  "Content-Type": "application/json",
+                                  "Auth-Token": authToken,
+                                  "Accept" : "application/json",
+                                },
+                                body: JSON.stringify(settings_json),
+                              };
+
+                              fetch(`${databaseUrl}settings/set`, options).then((res) =>
+                                res.json().then((data) => {
+                                  if (data["status"] != 200) {
+                                    console.log(data["status"] + " " + data["message"]);
+                                  }
+                                })
+                              );
+
+                            }}>
+                              {port}
+                              <X />
+                            </Button>
+                            ))}
+
+                            <div className="flex w-full max-w-sm items-center space-x-2">
+                              <Input id="d_newport" type="email" placeholder="Enter port #..." />
+                              <Button variant="outline" onClick={() => {
+
+                                let val = parseInt(document.getElementById("d_newport").value);
+                                if (Number.isInteger(val)) {
+                                  if(val >= 0 && val <= 65535) {
+                                    settings_json["d_ports"].push(val);
+                                    setPorts([...settings_json["d_ports"]]);
+                                    document.getElementById("d_newport").value = '';
+
+                                    const authToken = localStorage.getItem("Auth-Token");
+                                    if (authToken == null) {
+                                      console.log("User is logged out!");
+                                      return;
+                                    }
+      
+                                    const options = {
+                                      method: "PUT",
+                                      headers: {
+                                        "Content-Type": "application/json",
+                                        "Auth-Token": authToken,
+                                        "Accept" : "application/json",
+                                      },
+                                      body: JSON.stringify(settings_json),
+                                    };
+      
+                                    fetch(`${databaseUrl}settings/set`, options).then((res) =>
+                                      res.json().then((data) => {
+                                        if (data["status"] != 200) {
+                                          console.log(data["status"] + " " + data["message"]);
+                                        }
+                                      })
+                                    );
+                                    
+                                  } else {
+                                    throwCustomError("Enter a port between 0 and 65535");
+                                  }
+                                } else {
+                                  throwCustomError("Enter an integer");
+                                }
+                              }}
+                              >
+                                Add
+                              </Button>
+                            </div>
+
+                          </div>
+                        </PopoverContent>
+                      </Popover>
+
+
+                      
+                    </div>
+                    </CardContent>
+                  </Card>
+                <div className="flex justify-start items-center flex-wrap">
+                  <SettingsSwitch
+                      switchName="OS Scan"
+                      settingname="run_os"
+                      c={d_osSetting}
+                      onc={setdOS}
+                    />
+                    <SettingsSwitch
+                      switchName="Hostname Scan"
+                      settingname="run_hostname"
+                      c={d_hostnameSetting}
+                      onc={setdHostname}
+                    />
+                    <SettingsSwitch
+                      switchName="MAC Vendor Scan"
+                      settingname="run_mac_vendor"
+                      c={d_macvendorSetting}
+                      onc={setdMacVendor}
+                    />
+                    <SettingsSwitch
+                      switchName="Vertical Traceroute"
+                      settingname="run_vertical_trace"
+                      c={d_verttraceSetting}
+                      onc={setdVertTrace}
+                    />
+
+                    <div className="flex items-center justify-start space-x-2 w-1/3 p-4 m-0">
+                      <Label>Scan Interval</Label>
+                      
+                      <Input className={cn("w-1/3 text-right")} value={d_scanrateSetting} onChange={(e) => {
+                        if (Number.isInteger(parseInt(e.target.value))) {
+                          setdScanrate(parseInt(e.target.value));
+                          settings_json["d_scan_rate"] = parseInt(e.target.value);
+                        } else {
+                          setdScanrate(0);
+                          settings_json["d_scan_rate"] = 0;
+                        }
+                          const authToken = localStorage.getItem("Auth-Token");
+                          if (authToken == null) {
+                            console.log("User is logged out!");
+                            return;
+                          }
+
+                          const options = {
+                            method: "PUT",
+                            headers: {
+                              "Content-Type": "application/json",
+                              "Auth-Token": authToken,
+                              "Accept" : "application/json",
+                            },
+                            body: JSON.stringify(settings_json),
+                          };
+
+                          fetch(`${databaseUrl}settings/set`, options).then((res) =>
+                            res.json().then((data) => {
+                              if (data["status"] != 200) {
+                                console.log(data["status"] + " " + data["message"]);
+                              }
+                            })
+                          );
+
+                        }
+                      }
+                      />
+                      <Label>secs</Label>
+                    </div>
 
                   </div>             
                 </CardContent>
